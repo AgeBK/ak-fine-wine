5add52c4e6aa51224b151189926c86e98d865861
AgeBK <age24@hotmail.com>
2024-09-19 12:09:51 +1000

    modify fetchCarouselProducts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -278,6 +278,7 @@ export async function fetchCarouselProducts() {
     const products = data.rows;
     return cameliseArr(products);
   } catch (err) {
+    console.log(err);
     return [];
     // console.error('Database Error:', err);
     //  throw new Error('Failed to fetch carousel products.');
