cfde7eb7b91d38ba971f7de7a9ff949abc0c8cc1
AgeBK <age24@hotmail.com>
2024-09-20 08:51:49 +1000

    Include header and footer

--- app/ui/container/container.tsx
+++ app/ui/container/container.tsx
@@ -1,14 +1,14 @@
-// import Header from '@/app/ui/header';
-// import Footer from '@/app/ui/footer';
+import Header from '@/app/ui/header';
+import Footer from '@/app/ui/footer';
 import { ContainerProps } from '@/app/lib/definitions';
 import styles from '@/app/assets/css/Container.module.css';
 
 export default function Container({ children }: ContainerProps) {
   return (
     <div className={styles.container}>
-      {/* <Header /> */}
+      <Header />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
